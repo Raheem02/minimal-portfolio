8429aa1a29e4935bd7efe6f23288aeda4ebea344
Raheem02 <77abdulraheem@gmail.com>
2026-09-11 00:26:34 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,17 @@ export default function Document() {
       <Head>
         <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
         <meta httpEquiv="Referrer-Policy" content="strict-origin-when-cross-origin" />
+        <style dangerouslySetInnerHTML={{ __html: `
+          .nighty-night-shell { background-color: #17181c; }
+          .nighty-night-content { display: flex; flex-direction: column; }
+          @media (min-width: 1024px) {
+            .nighty-night-content { flex-direction: row; }
+            .nighty-night-content > aside { width: 20rem; flex-shrink: 0; }
+          }
+          @media (min-width: 1280px) {
+            .nighty-night-content > aside { width: 24rem; }
+          }
+        `}} />
       </Head>
       <body className="antialiased">
         <Main />
